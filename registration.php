c6a8bf28692d0c141dd5a862afb4748504b42977
natsraa <natsraa@gmail.com>
2017-09-05 17:08:14 +0700

    Add files via upload
    2017-09-05

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @Author: NNate Rungrueang
+ * @Date:   2017-09-05 15:09:20
+ * @Last Modified by:   nate
+ * @Last Modified time: 2017-07-11 15:09:28
+ * @website: http://natsra.com
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'natrans_th_th',
+    __DIR__
+);
